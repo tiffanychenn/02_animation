e7ebe02c8e872282c182b12e2cca97923952a1ad
Tiffany Chen <tiffanysta.c@gmail.com>
2018-02-08 15:48:11 -0500

    finished assignment i think

--- draw.js
+++ draw.js
@@ -2,26 +2,38 @@ var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.fillStyle = "#98DFAF";
 
-var b = document.getElementById("stop");
+var bstop = document.getElementById("stop");
+var bcircle = document.getElementById("circle");
+var bsquare = document.getElementById("square");
 
 var requestID = 0;
 var r = 0;
 var a = 1;
-var stopped = false;
 
-var animate = function(){
+var directionx = Math.floor(Math.random() * 10) - 5;
+var directiony = Math.floor(Math.random() * 10) - 5;
+var posx = 250;
+var posy = 250;
+
+var stopped = false;
+var started = false;
+var expanding;
+
+var animatec = function(){
+	started = true;
+	expanding = true;
 	stopped = false;
-	b.innerHTML = "stop";
-	animation(true);
+	bstop.innerHTML = "stop";
+	animationc(true);
 }
 
-var animation = function(bool){
+var animationc = function(bool){
 	pause();
 	if (bool){
 		r = 0;
 	}
 	var draw = function(){
-		r = findRadius();
+		findRadius();
 		ctx.clearRect(0, 0, 500, 500);
 		ctx.beginPath();
 		ctx.arc(250, 250, r, 0, 2 * Math.PI);
@@ -39,19 +51,52 @@ var findRadius = function(){
 		a = -1;
 	}
 	r += a;
-	return r;
+}
+
+var animates = function(){
+	started = true;
+	expanding = false;
+	stopped = false;
+	bstop.innerHTML = "stop";
+	animations(true);
+}
+
+var animations = function(bool){
+	pause();
+	if (bool){
+		directionx = Math.floor(Math.random() * 10) - 5;
+		directiony = Math.floor(Math.random() * 10) - 5;
+		posx = 250;
+		posy = 250;
+	}
+	var draw = function(){
+		ctx.clearRect(0, 0, 500, 500);
+		ctx.fillRect(posx - 10, posy - 10, 20, 20);
+		posx += directionx;
+		posy += directiony;
+		if (posy - 10 <= 0 || posy + 10 >= 500){
+			directiony = -1 * directiony;
+		}
+		if (posx - 10 <= 0 || posx + 10 >= 500){
+			directionx = -1 * directionx;
+		}
+		requestID = window.requestAnimationFrame(draw);
+	}
+	draw();
 }
 
 var stop = function(){
-	if (stopped) {
-		restart();
-		b.innerHTML = "stop";
+	if (started){
+		if (stopped) {
+			restart();
+			bstop.innerHTML = "stop";
+		}
+		else {
+			pause();
+			bstop.innerHTML = "continue";
+		}
+		stopped = !stopped;
 	}
-	else {
-		pause();
-		b.innerHTML = "continue";
-	}
-	stopped = !stopped;
 }
 
 var pause = function(){
@@ -59,8 +104,14 @@ var pause = function(){
 }
 
 var restart = function(){
-	animation(false);
+	if (expanding){
+		animationc(false);
+	}
+	else {
+		animations(false);
+	}
 }
 
-c.addEventListener('click', animate);
-b.addEventListener('click', stop);
+bstop.addEventListener('click', stop);
+bcircle.addEventListener('click', animatec);
+bsquare.addEventListener('click', animates);
